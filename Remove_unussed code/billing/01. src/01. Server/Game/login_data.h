0a47acbff1d87af630817d7b3882ed730d717cc1
IShungite <IShungite@gmail.com>
2021-11-30 19:26:10 +0100

    Removed RemainSecs

--- Remove_unussed code/billing/01. src/01. Server/Game/login_data.h
+++ Remove_unussed code/billing/01. src/01. Server/Game/login_data.h
@@ -9,8 +9,12 @@ search and delete:
 	void SetBilling(bool bOn);
 	bool IsBilling();
 
+	void SetRemainSecs(long l);
+	long GetRemainSecs();
+
 search and delete:
 
 	BYTE m_bBillType;
 	DWORD m_dwBillID;
 	bool m_bBilling;
+	long m_lRemainSecs;
